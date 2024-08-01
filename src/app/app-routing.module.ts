47287fca86a0a74b89d2c97bc5d3f7e1065bb372
Md-Shadab-Alam <shadabalam.1725@gmail.com>
2024-08-01 17:06:13 +0530

    created landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginPageComponent } from './component/login-page/login-page.component';
 import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { RegisterComponent } from './component/register/register.component';
+import { HomeComponent } from './component/home/home.component';
 
 const routes: Routes = [
-  {path:"",component:LoginPageComponent},
+  {path:"",component:HomeComponent},
+  {path:"register",component:RegisterComponent},
+  {path:"login",component:LoginPageComponent},
   {path:"dashboard",component:DashboardComponent},
   {path:"register",component:RegisterComponent}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 import { SearchComponent } from './component/search/search.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RegisterComponent } from './component/register/register.component';
+import { HomeComponent } from './component/home/home.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +18,8 @@ import { RegisterComponent } from './component/register/register.component';
     NavBarComponent,
     SearchComponent,
     LoginPageComponent,
-    RegisterComponent
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
